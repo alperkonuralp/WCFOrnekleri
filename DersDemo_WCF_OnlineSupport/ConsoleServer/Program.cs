ed829a29c68de96153ecfd863742ca6daaf389db
Alper Konuralp <alper@konuralp.gen.tr>
2010-06-20 12:18:01 +0300

    20.06.2010 tarihli değişiklikler 1

---
+++ DersDemo_WCF_OnlineSupport/ConsoleServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using OnlineSupportServiceLibrary;
+
+namespace ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OnlineSupportService obj =
+                new OnlineSupportService();
+
+            using (ServiceHost host = new ServiceHost(obj))
+            {
+                host.Open();
+
+                Console.WriteLine("Çıkış için enter'a basınız.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
---
+++ DersDemo_WCF_OnlineSupport/WindowsService/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.ServiceModel;
+using OnlineSupportServiceLibrary;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected ServiceHost _host;
+
+        protected override void OnStart(string[] args)
+        {
+            _host = new ServiceHost(
+                typeof(OnlineSupportService));
+
+            _host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            _host.Close();
+            ((IDisposable)_host).Dispose();
+            _host = null;
+        }
+
+    }
+}
