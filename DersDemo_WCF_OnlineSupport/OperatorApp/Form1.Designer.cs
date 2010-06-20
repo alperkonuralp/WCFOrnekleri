e2916d1933269afe977c9df656afff1e9a4a46fb
Alper Konuralp <alper@konuralp.gen.tr>
2010-06-20 15:16:41 +0300

    20.06.2010 - 2

---
+++ DersDemo_WCF_OnlineSupport/OperatorApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace OperatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 43);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 43);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(292, 230);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+
+    }
+}
+
---
+++ DersDemo_WCF_OnlineSupport/OperatorApp/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OperatorApp.OSS;
+using System.Threading;
+
+namespace OperatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            thGetOnlineUsers =
+                new Thread(GetOnlineClintsThreadMethod) { IsBackground = true };
+        }
+
+        public static string OperatorName;
+        public static Guid OperatorID;
+        public static OnlineSupportServiceClient Client =
+            new OnlineSupportServiceClient();
+
+        public static List<ClientData> Clients =
+            new List<ClientData>();
+
+        private Thread thGetOnlineUsers;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Startup s = new Startup();
+            if (s.ShowDialog(this) == DialogResult.OK)
+            {
+                OperatorName = s.OperatorName;
+
+                OperatorID = Client.OperatorStart(OperatorName);
+
+                thGetOnlineUsers.Start();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void GetOnlineClintsThreadMethod()
+        {
+            try
+            {
+                while (true)
+                {
+                    GetOnlineClients();
+                    Thread.Sleep(2000);
+                    GetChatData();
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+
+            }
+        }
+
+        private void GetOnlineClients()
+        {
+            ClientData[] clients = Client.GetClients();
+            foreach (var item in clients)
+            {
+                var esit = false;
+                foreach (var item2 in Clients)
+                {
+                    if (item.ClientID == item2.ClientID)
+                    {
+                        esit = true;
+                        item.OnlineTime = item2.OnlineTime;
+                        item.LastOperationTime = item2.LastOperationTime;
+                        break;
+                    }
+                }
+                if (!esit)
+                {
+                    // yeni client
+                    Clients.Add(item);
+                    CreateTabPage(item);
+                }
+            }
+        }
+
+        private DateTime _lastOperationTime =
+            DateTime.MinValue;
+
+        private Dictionary<Guid, UCChatControl> _ucChatControls =
+            new Dictionary<Guid, UCChatControl>();
+
+        private void GetChatData()
+        {
+            var cd = Client.OperatorGetMessages(
+                OperatorID, _lastOperationTime);
+
+            if (cd != null && cd.Length > 0)
+            {
+                foreach (var item in cd)
+                {
+                    Guid clientid;
+                    if (item.SenderType == SenderType.Operator)
+                    {
+                        // operatör yazmış.
+                        clientid = item.RecieverID;
+                    }
+                    else
+                    {
+                        // client yazmış
+                        clientid = item.SenderID;
+                    }
+
+                    UCChatControl uc;
+                    if (_ucChatControls.TryGetValue(
+                        clientid, out uc))
+                    {
+                        uc.WriteMessage(item);
+                    }
+                    _lastOperationTime = item.SendingTime;
+                }
+            }
+
+        }
+
+        public delegate void CreateTabPageDelegate(ClientData item);
+        private void CreateTabPage(ClientData item)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(
+                    new CreateTabPageDelegate(CreateTabPage),
+                    item);
+            }
+            else
+            {
+                TabPage tp = new TabPage(item.ClientName);
+                tp.Tag = item;
+
+                UCChatControl uc = new UCChatControl();
+
+                tp.Controls.Add(uc);
+                uc.Location = new Point(0, 0);
+                uc.Dock = DockStyle.Fill;
+                uc.CData = item;
+                _ucChatControls.Add(item.ClientID, uc);
+
+                tabControl1.TabPages.Add(tp);
+            }
+        }
+    }
+}
